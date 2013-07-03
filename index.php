dfd1e905dffae9614fd643e76bb8ead96e0854be
supachai_chai@hotmail.com <supachai_chai@hotmail.com>
2013-07-03 17:46:27 +0700

    + Add header code.

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
-hello world!!
+<?php
+/**
+ * @version			$Id: $
+ * @package			Joomla
+ * @subpackage		Module XXX
+ * @copyright		Copyright (C) 2013 Marvelic Engine Co.,Ltd..CMSPlugin.com http://marvelic.co.th All rights reserved.
+ * @license			GNU General Public License version 3 or later
+ *
+ * Marvelic Engine Co.,Ltd. is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 
-
-commit v2
-
-+++ append by zero
-
-Nokkaew here 0/
+//No direct access
+defined('_JEXEC') or die('Restricted access');
